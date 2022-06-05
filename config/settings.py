4fd1331c648dca537368c90440fccf2c8876c32a
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-06-05 02:05:57 -0400

    solve-html-broken

--- config/settings.py
+++ config/settings.py
@@ -78,19 +78,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-        'default': dj_database_url.config(
-            default=config('DATABASE_URL')
-            )
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# DATABASES = {
+#         'default': dj_database_url.config(
+#             default=config('DATABASE_URL')
+#             )
+#         }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
