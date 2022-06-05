e6471f4e6ea47debe7831413057d6c60b18464e2
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-06-05 00:49:18 -0400

    db-heroku-settings

--- config/settings.py
+++ config/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#         'default': dj_database_url.config(
-#             default=config('DATABASE_URL')
-#             )
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+            )
+        }
 
 
 # Password validation
