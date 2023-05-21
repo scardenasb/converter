b49d0158a44b0633a267ccf8ca8fcb64ddbd54a6
scardenasb <84429399+scardenasb@users.noreply.github.com>
2023-05-21 05:16:11 -0400

    update

--- converter/admin.py
+++ converter/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from .models import Unit, Type
-
-# Register your models here.
-
-admin.site.register(Unit)
-admin.site.register(Type)
+#from django.contrib import admin
+#from .models import Unit, Type
+#
+## Register your models here.
+#
+#admin.site.register(Unit)
+#admin.site.register(Type)
